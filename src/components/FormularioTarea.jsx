ef5831e98e6da6f0a28f48c1de1d491cdb481ba7
EduRossB <edu_barba@outlook.com>
2022-11-01 22:55:35 -0300

    agregamos ciclo de vida con localStorage

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
 
 const FormularioTarea = () => {
   // aqui va la logica
-
+  const listaLocalStorage =
+    JSON.parse(localStorage.getItem("listaTarea")) || [];
   const [tarea, setTarea] = useState("");
-  const [arregloTareas, setArregloTareas] = useState([]);
+  const [arregloTareas, setArregloTareas] = useState(listaLocalStorage);
+
+  // CICLO DE VIDA
+  useEffect(() => {
+    console.log("Prueba ciclo de vida");
+    localStorage.setItem("listaTarea", JSON.stringify(arregloTareas));
+  }, [arregloTareas]);
+  // use efect con corchetes vacios como segundo parametro hace que solo se active cuando se monta, no cuando se actualiza
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -14,10 +22,10 @@ const FormularioTarea = () => {
     setTarea("");
   };
 
-const borrarTarea = (nombre)=>{
-  let arregloModificado = arregloTareas.filter((item)=> item !== nombre);
-  setArregloTareas(arregloModificado)
-}
+  const borrarTarea = (nombre) => {
+    let arregloModificado = arregloTareas.filter((item) => item !== nombre);
+    setArregloTareas(arregloModificado);
+  };
 
   return (
     <div>
@@ -36,7 +44,10 @@ const borrarTarea = (nombre)=>{
         </Form.Group>
       </Form>
 
-      <ListaTareas propsArregloTareas={arregloTareas} borrarTarea={borrarTarea}></ListaTareas>
+      <ListaTareas
+        propsArregloTareas={arregloTareas}
+        borrarTarea={borrarTarea}
+      ></ListaTareas>
     </div>
   );
 };
